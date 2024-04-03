2c6a04262b56bc4323cc99f333c23c8f4f7953ec
KevinEHernandez <146020060+KevinEHernandez@users.noreply.github.com>
2024-04-02 21:55:33 -0500

    Update main.cpp

--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -3,47 +3,49 @@
 #include <fstream>
 #include <vector>
 #include "header.h"
+#include <sstream>
 using namespace std;
 
 int main() {
-    vector<double_vector> my_double_vector;
+    vector<double_vector> vectors;
     vector<my_distance> all_pairs_distances;
 
     ifstream in_file;
     in_file.open("vectors.txt");
 
     int id = 0;
-    double from, to;
+    string line;
 
-    while (in_file >> from >> to) {
-        double_vector d_vector = double_vector(from, to, id++);
-        my_double_vector.push_back(d_vector);
+    while (getline(in_file, line)) {
+        stringstream ss(line);
+        double value;
+        vector<double> vals;
+        while (ss >> value) {
+            vals.push_back(value);
+        }
+        vectors.push_back(double_vector(vals, id));
+        id++;
+    }
+    
+    for (size_t i = 0; i < vectors.size(); ++i) {
+        for (size_t j = i + 1; j < vectors.size(); ++j) {
+            double distance = cosine_distance(vectors[i], vectors[j]);
+            all_pairs_distances.push_back(my_distance(i, j, distance));
+        }
     }
 
-    for (auto vect : my_double_vector) {
-        cout << vect.x << " " << vect.y << " cos dist with itself: " << cosine_distance(vect, vect) << endl;
+    for (auto dist : all_pairs_distances) {
+        cout << " " << dist.id_1 << " " << dist.id_2 << " dist = " << dist.cos_distance << endl;
     }
 
     cout << endl;
 
-    int size = my_double_vector.size();
-
-    for (int i = 0; i < size; i++) {
-        for (int j = i + 1; j < size; j++) {
-            my_distance my_distance1(i, j, cosine_distance(my_double_vector[i], my_double_vector[j]));
-            all_pairs_distances.push_back(my_distance1);
-        }
-    }
-
-    sort(all_pairs_distances.begin(),
-         all_pairs_distances.end(),
-         [](const my_distance & left, const my_distance & right) -> bool {
-             return left.cos_distance < right.cos_distance;
-         });
-
+    sort(all_pairs_distances.begin(), all_pairs_distances.end(), [](const my_distance & left, const my_distance & right) {
+        return left.cos_distance < right.cos_distance;
+    });
 
     for (auto dist : all_pairs_distances) {
-        cout << dist.id_1 << " " << dist.id_2 << " cos dist = " << dist.cos_distance << endl;
+        cout << " " << dist.id_1 << " " << dist.id_2 << " cos dist = " << dist.cos_distance << endl;
     }
 
     cout << endl;
@@ -52,3 +54,4 @@ int main() {
 
     return 0;
 }
+
